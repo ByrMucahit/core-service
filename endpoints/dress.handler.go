4b413c49ceefc21be0043edcca1b6a8077e203da
mucahitbayar-ins <mucahit.bayar@useinsider.com>
2025-02-08 22:33:38 +0300

    feature: "update dress.handler.go to create new handlers to handle CRUD transactions"

--- endpoints/dress.handler.go
+++ endpoints/dress.handler.go
@@ -6,18 +6,27 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type DressRequest struct {
-	Url         string                 `json:"url"`
+	Url         string
 	Description string                 `json:"description"`
 	Labels      []string               `json:"labels"`
 	Weather     float64                `json:"weather"`
 	Location    map[string]interface{} `json:"location" gorm:"type:jsonb"`
+	id          uint
 }
 
-func CreateDressHandler(c *gin.Context) {
-	dressService := service.DressServiceInstance()
+type DressHandler struct {
+	DressService *service.DressService
+}
+
+func NewDressHandler(service *service.DressService) *DressHandler {
+	return &DressHandler{DressService: service}
+}
+
+func (h *DressHandler) CreateDressHandler(c *gin.Context) {
 	var req DressRequest
 
 	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
@@ -33,10 +42,47 @@ func CreateDressHandler(c *gin.Context) {
 		Location:    locationJSON,
 	}
 
-	err := dressService.AddDress(dress)
+	err := h.DressService.AddDress(dress)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusCreated, gin.H{"message": "Product created successfully"})
+}
+
+func (h *DressHandler) GetAllDresses(c *gin.Context) {
+	dresses, err := h.DressService.FindDress()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch dresses"})
+		return
+	}
+	c.JSON(http.StatusOK, dresses)
+}
+
+func (h *DressHandler) UpdatePartialDressById(c *gin.Context) {
+	var req map[string]interface{}
+	id, _ := strconv.Atoi(c.Param("id"))
+	var entity = &models.Dress{}
+
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.DressService.UpdatesPartial(entity, uint(id), req); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Entity updated successfully"})
+}
+
+func (h *DressHandler) DeletePartialDressById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var entity = &models.Dress{}
+
+	if err := h.DressService.DeletePartialDress(entity, uint(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to Delete"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Entity Deleted successfully"})
 }
